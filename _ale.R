68ca1cb4dc6e6621b3e2275943463164308900ec
Patrick Sadil <psadil@gmail.com>
2022-01-19 20:20:43 -0500

    last week update

--- _ale.R
+++ _ale.R
@@ -9,8 +9,6 @@ source(here::here("R", "ale.R"))
 library(future)
 # library(future.callr)
 # plan(callr)
-library(fst)
-library(qs)
 library(future.batchtools)
 plan(batchtools_sge, template = "tools/sge.tmpl")
 
@@ -25,14 +23,14 @@ list(
     feat_dirs,
     readr::read_lines(avail, num_threads=1),
     format = "qs"),
-  tar_target(n_sub, c(10, 30)),
-  tar_target(n_study, c(10, 30)),
-  tar_target(iter, seq_len(1)),
-  tar_target(cope5_index, seq_len(900)),
+  tar_target(n_sub, c(10, 20, 30, 50, 100, 250, 500, 1000)),
+  tar_target(n_study, c(1)),
+  tar_target(iter, seq_len(50)),
+  tar_target(cope5_index, seq_len(10000)),
   tar_target(
     cope5,
     apply_reg_cope(feat_dirs[cope5_index], tar_path()),
-    format = "file",
+    format = "file", error="continue",
     pattern = map(cope5_index)),
   tar_target(
     cope_files,
@@ -50,30 +48,36 @@ list(
     t_img,
     do_t(cope_files2),
     pattern = map(cope_files2),
-    format = "fst_tbl"),
-  tar_target(
-    ale_py_script,
-    fs::path("python", "ale.py"),
-    format = "file"),
-  tar_target(
-    t_img2,
-    t_img |>
-      dplyr::group_by(iter, n_study, n_sub) |>
-      tar_group(),
-    iteration = "group",
-    format = "fst_tbl"),
-  tar_target(
-    ale,
-    do_ale_py(t_img2, python_source = here::here(ale_py_script), condaenv = "meta"),
-    cue = tar_cue(depend = FALSE),
-    pattern = map(t_img2),
     format = "fst_tbl",
     resources = tar_resources(
       future = tar_resources_future(
         plan = tweak(
           batchtools_sge,
           template = "tools/sge.tmpl",
-          resources = list(mem_free = "5G"))))),
+          resources = list(mem_free = "15G"))))),
+  tar_target(
+    ale_py_script,
+    fs::path("python", "ale.py"),
+    format = "file"),
+#  tar_target(
+#    t_img2,
+#    t_img |>
+#      dplyr::group_by(iter, n_study, n_sub) |>
+#      tar_group(),
+#    iteration = "group",
+#    format = "fst_tbl"),
+#  tar_target(
+#    ale,
+#    do_ale_py(t_img2, python_source = here::here(ale_py_script), condaenv = "meta"),
+#    cue = tar_cue(depend = FALSE),
+#    pattern = map(t_img2),
+#    format = "fst_tbl",
+#    resources = tar_resources(
+#      future = tar_resources_future(
+#        plan = tweak(
+#          batchtools_sge,
+#          template = "tools/sge.tmpl",
+#          resources = list(mem_free = "5G"))))),
   tar_target(
     z_pop,
     calc_z(cope5),
@@ -83,25 +87,25 @@ list(
         plan = tweak(
           batchtools_sge, 
           template = "tools/sge.tmpl", 
-          resources = list(mem_free = "30G"))))),
-  tar_target(
-    comparison,
-    tidy_ale(ale),
-    pattern = map(ale),
-    format = "fst_tbl"
-  ),
-  tar_target(
-    ibma_py_script,
-    fs::path("python", "ibma.py"),
-    format = "file"),
-  tar_target(
-    ibma,
-    do_ibma_py(ale, python_source = here::here(ibma_py_script), condaenv = "meta"),
-    pattern = map(ale),
-    format = "fst_tbl"),
-  tar_target(
-    ibma_summary,
-    tidy_ibma(ibma),
-    pattern = map(ibma),
-    format = "fst_tbl")
+          resources = list(mem_free = "30G")))))
+#  tar_target(
+#    comparison,
+#    tidy_ale(ale),
+#    pattern = map(ale),
+#    format = "fst_tbl"
+#  ),
+#  tar_target(
+#    ibma_py_script,
+#    fs::path("python", "ibma.py"),
+#    format = "file"),
+#  tar_target(
+#    ibma,
+#    do_ibma_py(ale, python_source = here::here(ibma_py_script), condaenv = "meta"),
+#    pattern = map(ale),
+#    format = "fst_tbl"),
+#  tar_target(
+#    ibma_summary,
+#    tidy_ibma(ibma),
+#    pattern = map(ibma),
+#    format = "fst_tbl")
 )
