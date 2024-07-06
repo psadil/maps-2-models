6e0b4ebe8eb92fbf1262ece8cbc5740f40762622
Patrick Sadil <psadil@gmail.com>
2024-07-05 20:23:21 -0400

    add unrestricted hcp

--- tools/bundle-hcp.py
+++ tools/bundle-hcp.py
@@ -1,10 +1,6 @@
 import polars as pl
 
-u = pl.read_csv(
-    "../data-raw/act_preds/data/unrestricted_psadil_2_20_2023_14_49_6.csv"
-)
-r = pl.read_csv(
-    "../data-raw/act_preds/data/RESTRICTED_martin_5_16_2021_16_53_10.csv"
-)
+u = pl.read_csv("../data-raw/hcp/unrestricted.csv")
+r = pl.read_csv("../data-raw/hcp/restricted.csv")
 
 u.join(r, on="Subject").write_parquet("../data-raw/hcp.parquet")
